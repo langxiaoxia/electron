91af7ad50f6ae95faedbd350c8c9367f93d0d4fc
Xiaoxia Lang <xxlang@grandstream.cn>
2021-12-16 11:58:25 +0800

    Auto use current date as electron build tag.

--- script/generate-version-json.js
+++ script/generate-version-json.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
 
+//+by xxlang@2021-12-15 {
+const moment = require('moment');
+moment.locale('zh-cn');
+const tag = moment().format('YYYYMMDD');
+//+by xxlang@2021-12-15 }
+
 const outputPath = process.argv[2];
 
 const currentVersion = fs.readFileSync(path.resolve(__dirname, '../ELECTRON_VERSION'), 'utf8').trim();
@@ -20,6 +26,7 @@ const {
 } = parsed;
 
 fs.writeFileSync(outputPath, JSON.stringify({
+  tag, //+by xxlang@2021-12-09
   major,
   minor,
   patch,
